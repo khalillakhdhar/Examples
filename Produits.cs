de570241cab171a01cf7298e7af23cf90eb39e82
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2026-06-12 16:39:16 +0100

    repo

---
+++ Produits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    internal class Produits
+    {
+        public string Nom { get; set; }
+        public string Marque { get; set; }
+        public double Prix { get; set; }
+        public int Quantite { get; set; }
+        public bool Cassable { get; set; }
+
+        public Produits(string nom, string marque, double prix, int quantite, bool cassable)
+        {
+            Nom = nom;
+            Marque = marque;
+            Prix = prix;
+            Quantite = quantite;
+            Cassable = cassable;
+        }
+
+        public override string? ToString()
+        {
+            return $"Nom: {Nom} Marque: {Marque} Prix: {Prix}, Quantite {Quantite}, Cassable :{Cassable}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            string reponse= "oui";
+            while (reponse.ToLower() != "non")
+            {
+                //exemple 1
+                Console.WriteLine("Entrez votre âge :");
+                int age = Int32.Parse(Console.ReadLine());
+                string statut = age >= 18 ? "Majeur" : "Mineur";
+                Console.WriteLine(statut); // Affiche : Majeur
+                                           // Équivalent avec if/else :
+                Console.WriteLine("Continuer? oui/non :");
+                reponse = Console.ReadLine();
+               
+
+
+
+        } 
+            int[] nombres = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var pairs = nombres.Where(n => n % 2 == 0);
+            foreach (var pair in pairs)
+            {
+                Console.WriteLine(pair);
+            }
+            
+            string[] joueurs = ["Ahmed", "Ali", "Sami", "Raja", "Mahmoud", "Khalil"];
+            var longNom = joueurs.Where(j => j.Length > 4);
+            foreach (string joueur in longNom)
+            {
+                Console.WriteLine(joueur);
+            }
+            */
+            Produits[] produits = {
+                new Produits("pc", "lenovo", 3200, 4, true),
+                new Produits("pc", "dell", 4200, 0, true),
+                new Produits("flashDisque","traceData",2,20,false)
+
+
+
+
+            };
+            Console.WriteLine("liste des produits");
+            var productSummary = produits.Select(p => $"{p.Nom} / {p.Marque}");
+            foreach(var produit in productSummary)
+                Console.WriteLine(produit);
+            var cassables = produits.Where(p => p.Cassable);
+            Console.WriteLine("liste des produits cassable");
+
+            foreach (var produit in cassables)
+                Console.WriteLine(produit.ToString());
+        }
+        
+    }
+}
